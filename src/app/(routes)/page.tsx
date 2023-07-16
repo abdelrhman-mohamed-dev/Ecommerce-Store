65f71e01a7741ec70ca53c9257f82c7828c09780
boody-cpu <81966035+boody-cpu@users.noreply.github.com>
2023-07-16 19:30:00 +0300

    enviroment setup and home page with billboards and featured products

---
+++ src/app/(routes)/page.tsx
@@ -0,0 +1,23 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductsList from "@/components/products-list";
+import Container from "@/components/ui/container";
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("5601db1d-fdfb-4425-9ec9-055fa9babd3b");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductsList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black ">
+          &copy; 2023 Mirror, Inc. All rights resrved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
